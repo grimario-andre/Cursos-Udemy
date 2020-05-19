90a867eba1ff2df8b54bcab60acbf6d76e3dda5b
grimario-andre <andre02souza@outlook.com.br>
2020-05-19 14:08:37 -0300

    Operations logics

---
+++ Algorithms/Exercises/operadores_Logicos.js
@@ -0,0 +1,28 @@
+/*
+   // Vereficando se o aluno esta ou nao aprovado.
+   se media >= 7 entao
+      Escreval("Aluno Aprovado")
+   senao
+      Escreval("Aluno Reprovado")
+   fimse
+*/
+var nome, nota01, nota02, media;
+
+nome = prompt("Informe o nome do aluno ");
+nota01 = prompt("Digite a nota 01 do aluno ");
+nota02 = prompt("Digite a nota 02 do aluno ");
+passou = false;
+
+// Calculo da média, se faz necessário converter as var nota01 e 02 de string para inteiro.
+media = parseFloat(nota01 + nota02) / 2;
+alert("A nota do aluno é: " + media);
+
+// Vereficando se o aluno esta ou nao aprovado.
+if (media >= 5) {
+    passou = true;
+}
+ if (passou && (media >= 50 && media <= 90)) {
+    alert("Aprovado aluno: " + nome);
+ } else {
+    alert("Reprovado aluno: " + nome);
+ }
