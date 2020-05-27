d887b26ce84427d133fb5275dc3e5cdae381908c
grimario-andre <andre02souza@outlook.com.br>
2020-05-27 19:05:11 -0300

    Math operations.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,19 @@
-var mensagem = "Hello Wolrd of Javascript!";
-alert(mensagem);
+//math operations.
+
+var a , b, soma, sub, multi, div;
+
+a = 2;
+b = 3;
+
+soma = a + b;
+
+sub = b - a ;
+
+multi = a * b;
+
+div = b / a;
+
+alert(soma);
+alert(sub);
+alert(multi);
+alert(div);
