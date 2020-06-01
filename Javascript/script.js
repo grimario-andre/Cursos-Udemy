80f39ea80ded0d7d9a99937895d147539ecdc8f1
grimario-andre <andre02souza@outlook.com.br>
2020-06-01 18:04:59 -0300

    Validate Form.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,11 +1,29 @@
-//Events DOM
+//valdiate form.
 
-function mudacor(cor) {
-    var elemento = document.getElementById("mensagem");
-    elemento.style.backgroundColor = cor;
+function validanome(nome) {
+    var nome = document.getElementById("nome");
+
+    if (nome.value == '') {
+        alert("O campo nome não pode estar em branco! ");
+    } else {
+        alert("Formulário enviado com sucesso! ");
+    }
 }
 
-function cortexto(ctext) {
-    var elemento = document.getElementById('mensagem');
-    elemento.style.color = ctext;
+//validate birth.
+
+function validaidade(idade) {
+    var idade = document.getElementById("idade");
+    
+    if (idade.value == '') {
+        alert("Insira o ano de nascimento! ");
+    } else {
+        if ((idade.value > 2020) || (idade.value <= 1920)) {
+            alert("Ano inválido! ");
+        } else {
+            idade = 2020 - idade;
+        }
+    }
+    
+    return idade;
 }
