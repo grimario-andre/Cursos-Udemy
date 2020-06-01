a4d02b967e65c3a7caab0ee3be43a004fa210f37
grimario-andre <andre02souza@outlook.com.br>
2020-06-01 12:05:25 -0300

    Estructure of looping (for)

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,19 +1,5 @@
-//condition structure.
+//looping.
 
-var a;
-
-prompt("Digite um número: ", a);
-
-if (a % 2 == 0) {
-    alert("Número dividido por 2 com resto zero na divisão. ");
-} else {
-    if (a % 3 == 0) {
-        alert("Número dividido por 3 com resto zero.");
-    } else {
-        if ((a % 2 == 0) && (a % 3 == 0)) {
-            alert("Número é divisível por 2 e 3 com resto zero. ");
-        } else {
-            alert("Número não divisível por 2 e 3 com resto zero. ");
-        }
-    }
+for(i=0; i <5; i ++){
+    alert(i);
 }
