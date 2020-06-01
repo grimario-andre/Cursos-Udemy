32c92618ad838fb2192b9f813a749ee8f1b8beb9
grimario-andre <andre02souza@outlook.com.br>
2020-06-01 14:39:22 -0300

    Structure of looping (while and do while)

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,18 @@
 //looping.
 
-for(i=0; i <5; i ++){
+for (i = 0; i < 5; i++) {
     alert(i);
 }
+
+var j = 10;
+while (j >= 0) {
+    alert(j);
+
+    j = j - 1;
+}
+
+var i = 0;
+do {
+    alert(i)
+    i = i + 1;
+} while (i <= 5);
