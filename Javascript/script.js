aab18aa00af9b73a61b8dd7432d7410a25f66dbf
grimario-andre <andre02souza@outlook.com.br>
2020-06-03 17:09:05 -0300

    Working with efect fade.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
     $("#azul").click(function(){
         $("p").css("color","blue");
+        $("p").fadeOut(5000);
+        $("p").delay(3000);
+        $("p").fadeIn(2000);
     });
 
     $("#vermelho").click(function(){
         $("p").css("color","red");
+        $("p").fadeOut("slow");
+        $("p").delay(5000);
+        $("p").fadeIn();
     });
 
     $("#esconder").click(function(){
         $("h1").hide();
+        $("h1").fadeIn();
     });
 
     $("#azul").click(function(){
