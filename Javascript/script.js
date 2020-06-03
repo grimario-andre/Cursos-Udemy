11f8fd29fcec3d6f6fb66cf79742f6f4fe51df95
grimario-andre <andre02souza@outlook.com.br>
2020-06-03 17:22:51 -0300

    block chaining

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
     $("#azul").click(function(){
-        $("p").css("color","blue");
-        $("p").fadeOut(5000);
-        $("p").delay(3000);
-        $("p").fadeIn(2000);
+        $("p").css("color","blue")
+        .fadeOut(5000)
+        .delay(3000)
+        .fadeIn(2000);
     });
 
     $("#vermelho").click(function(){
-        $("p").css("color","red");
-        $("p").fadeOut("slow");
-        $("p").delay(5000);
-        $("p").fadeIn();
-        $("#mensagem").text("Cor alterada com sucesso.");
-        $("#mensagem").css("color","red");
-        $("#mensagem").fadeOut(2000);
+        $("p").css("color","red")
+        .fadeOut("slow")
+        .delay(5000)
+        .fadeIn();
+
+        $("#mensagem")
+        .text("Cor alterada com sucesso.")
+        .css("color","red")
+        .fadeOut(2000);
     });
 
     $("#esconder").click(function(){
-        $("h1").hide(2000);
-        $("h1").fadeIn("slow");
+        $("h1").hide(2000)
+        .fadeIn("slow");
     });
 
     $("#azul").click(function(){
