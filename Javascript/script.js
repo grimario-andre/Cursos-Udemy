a91076c842740d933d4377796e959f47c71624ad
grimario-andre <andre02souza@outlook.com.br>
2020-06-03 14:30:04 -0300

    woking with jquery.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,29 +1,7 @@
-//valdiate form.
+//working with jquery.
 
-function validanome(nome) {
-    var nome = document.getElementById("nome");
-
-    if (nome.value == '') {
-        alert("O campo nome não pode estar em branco! ");
-    } else {
-        alert("Formulário enviado com sucesso! ");
-    }
-}
-
-//validate birth.
-
-function validaidade(idade) {
-    var idade = document.getElementById("idade");
-    
-    if (idade.value == '') {
-        alert("Insira o ano de nascimento! ");
-    } else {
-        if ((idade.value > 2020) || (idade.value <= 1920)) {
-            alert("Ano inválido! ");
-        } else {
-            idade = 2020 - idade;
-        }
-    }
-    
-    return idade;
-}
+$(document).ready(function(){
+    $('button').click(function(){
+        $('h1').hide();
+    })
+});
