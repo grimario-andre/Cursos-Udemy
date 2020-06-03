3d792fcd354d3817e2162cddf7b18dc0717097c9
grimario-andre <andre02souza@outlook.com.br>
2020-06-03 15:41:38 -0300

    changing the css with jquery

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,22 @@
-//working with jquery.
-
 $(document).ready(function(){
-    $('button').click(function(){
-        $('h1').hide();
-    })
+    $("#azul").click(function(){
+        $("p").css("color","blue");
+    });
+
+    $("#vermelho").click(function(){
+        $("p").css("color","red");
+    });
+
+    $("#esconder").click(function(){
+        $("h1").hide();
+    });
+
+    $("#azul").click(function(){
+        $("p").css("background-color","white");
+    });
+
+    $("#vermelho").click(function(){
+        $("p").css("background-color","yellow");
+    });
 });
+
